9ffe85afd429bc387e3172982fd3d544dc4db918
Henry Tirla <henrytirla@gmail.com>
2022-12-08 00:38:42 +0000

    Create wallet address

---
+++ BNB Chain/create-multiple-BSCaddress.py
@@ -0,0 +1,18 @@
+
+from web3 import Web3
+
+# Connect to the Ethereum blockchain using the Infura node
+#web3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/your-infura-api-key'))
+
+web3 = Web3(Web3.HTTPProvider("Enter node here "))
+# Define the number of addresses to generate
+num_addresses = 10
+
+# Generate the addresses and private keys
+for i in range(num_addresses):
+    # Generate a new Ethereum address and private key
+    address = web3.eth.account.create()
+
+    # Print the address and private key
+    print(f"Address: {address.address}")
+    print(f"Private key: {address.privateKey.hex()}")
