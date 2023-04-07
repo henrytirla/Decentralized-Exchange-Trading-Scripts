c4bf9a211cf23a63cd0ae87db00faab0c3fa12db
Henry Tirla <henrytirla@gmail.com>
2023-04-07 01:49:14 +0100

    Add mnemonic notice

--- BNB Chain/Hd-wallet.py
+++ BNB Chain/Hd-wallet.py
@@ -5,7 +5,17 @@ from mnemonic import Mnemonic
 from web3 import Web3
 import secrets
 from eth_account import Account
+"""
+Please note the public key might differ when importing into different wallet services 
+like TrustWallet or SafePal because these services might use different derivation paths.
 
+Popular derivation paths include:
+
+BIP44: m/44'/coin_type'/account'/change/address_index
+BIP49: m/49'/coin_type'/account'/change/address_index
+BIP84: m/84'/coin_type'/account'/change/address_index
+
+"""
 
 w3 = Web3(Web3.HTTPProvider("https://bsc-dataseed1.binance.org/"))
 
@@ -22,7 +32,9 @@ accout = w3.eth.account.privateKeyToAccount(acct_privatekeys)
 
 account_publickey = accout.address
 account_privatekey= accout.privateKey
-
+# print("MNEMONIC ADDRESS-- Public Key",acct.address)
+# print("MNEMONIC PRIVATE KEY---[KEEP PRIVATE]",w3.toHex(acct.privateKey))
 print("MNEMONIC PHRASE--[KEEP PRIVATE]",words)
+
 print('Public Key', account_publickey)
 print("Private_Key---[KEEP PRIVATE]",w3.toHex(account_privatekey))
