b08dd79fc056e200cbddb9fe4c2cef6c4b7fb686
Henry Tirla <henrytirla@gmail.com>
2023-06-26 09:04:57 +0100

    Track BNB Buys dispatcher

---
+++ BNB Chain/Trading/TrackEOA_ReceivingBNB.py
@@ -0,0 +1,1 @@
+#TODO Track the contract address of BNB smartcontract dispatcher. The aim is to find new wallets that receive BNB ie Lowe nonce count and the aggregae them in a pool and monitor them for new transactions emmited
