bdadd06d4f25a23d9b571261f07db68755bbddc8
Henry Tirla <henrytirla@gmail.com>
2023-07-04 05:40:51 +0100

    Iteration

--- Ethereum/Memepool/AllrouterAddresses.py
+++ Ethereum/Memepool/AllrouterAddresses.py
@@ -1,14 +1,15 @@
 "Scan Memepool for all router addresses."
 
-#DoingNOW
-#Get Inputs and feed it to the decode_input
-#Handle each input sequentially
+
 
 import asyncio
 from web3 import Web3
 import json
-#from eth_abi import decode_abi
 from uniswap_universal_router_decoder import RouterCodec
+
+
+
+
 
 class style():  # Class of different text colours - default is white
     BLACK = '\033[30m'
@@ -22,7 +23,7 @@ class style():  # Class of different text colours - default is white
     UNDERLINE = '\033[4m'
     RESET = '\033[0m'
 
-w3 = Web3(Web3.HTTPProvider(""))
+w3 = Web3(Web3.HTTPProvider("Enter your node"))
 
 
 uniswap_router_addresses = [
@@ -35,35 +36,80 @@ uniswap_router_addresses = [
 
 ]
 
+check_functions = {"swapETHForExactTokens": True, "swapExactETHForTokens": True,
+                   "swapExactETHForTokensSupportingFeeOnTransferTokens": True, "addLiquidityETH": True,
+                   'multicall': True, 'execute': True}
+
+
+def get_FunctionName(decoded_data):
+    decoded_str = str(decoded_data[0])
+    start_index = decoded_str.find(" ") + 1
+    end_index = decoded_str.find("(")
+    function_name = decoded_str[start_index:end_index]
+    return function_name
+
+# uniswap_v3_path = b"\xc0*\xaa9\xb2#\xfe\x8d\n\x0e ... \xd7\x89"  # bytes or str hex
+# fn_name = "V3_SWAP_EXACT_IN"  # Or V3_SWAP_EXACT_OUT
+# codec = RouterCodec()
+# decoded_path = codec.decode.v3_path(fn_name, uniswap_v3_path)
+def decode_execute(decode_data):
+    codec = RouterCodec()
+    decoded_trx_input = codec.decode.function_input(decode_data)
+    In_put = decoded_trx_input[1]['inputs'][1]
+    fn_name= get_FunctionName(In_put)
+    print(fn_name)
+    if fn_name == 'V3_SWAP_EXACT_IN':
+       #fn_name = "V3_SWAP_EXACT_IN"
+       print(decoded_trx_input)
+       path = In_put[1]['path']
+       decoded_path = codec.decode.v3_path(fn_name, path)
+       return fn_name,decoded_path
+    elif fn_name == 'V2_SWAP_EXACT_IN':
+         print(decoded_trx_input)
+         v2_path= In_put[1]['path']
+         return v2_path
+    elif fn_name == "UNWRAP_WETH":
+         print(decoded_trx_input)
+         data= decoded_trx_input[1]['inputs'][0][1]['path']
+         return fn_name,data
+
+
+
+
+
+
 
 
 def decode_input_data(input_data, router):
     with open("router_abi.json") as f:
         routers_abi = json.load(f)
-    #print(routers_abi)
-
-
     abi = routers_abi.get(router)
-    #print(abi)
 
     if abi is None:
         raise ValueError("ABI not found for the specified router.")
-
     contract = w3.eth.contract(address=router, abi=abi)
     decoded_data = contract.decode_function_input(input_data)
-    #print(decoded_data)
-    decoded_str = str(decoded_data[0])
-    start_index = decoded_str.find(" ") + 1
-    end_index = decoded_str.find("(")
-    function_name = decoded_str[start_index:end_index]
-    print(style.CYAN+function_name)
-    if function_name == "execute":
-        codec = RouterCodec()
-        decoded_trx_input = codec.decode.function_input(input_data)
-        return decoded_trx_input
 
-    else:
-       return decoded_data
+
+    function_name = get_FunctionName(decoded_data)
+
+
+    if function_name in check_functions and check_functions[function_name]:
+        print(style.CYAN + function_name)
+        if function_name == "execute":
+
+            data= decode_execute(input_data)
+            # return  SwapExact_in , In_put ,decoded_trx_input
+            return data
+        elif function_name == "multicall":
+            byte_data = decoded_data[1]['data'][0]
+            multicall_decoded = contract.decode_function_input(byte_data)
+            return multicall_decoded
+        else:
+            path_ =decoded_data[1]['path']
+            return path_
+    # else:
+    #     return "Pass"
 
 
 
@@ -75,22 +121,24 @@ def decode_input_data(input_data, router):
 async def handle_new_block(block):
     for tx_hash in block['transactions']:
         try:
-           # tx = w3.eth.getTransaction(tx_hash)
+
             tx = w3.eth.get_transaction(tx_hash)
 
 
             for router_address in uniswap_router_addresses:
                 if tx['to'] == router_address:
-                    # Process the transaction
-                    #decoded_input_data = decode_input_data(tx['input'])
-                    #tx_hash = w3.toHex(tx)
                     hash= tx['hash']
-                    #tx_hash = w3.toHex(hash)
                     tx_hash = w3.to_hex(hash)
-                    #print(tx_hash)
-                    print(style.YELLOW+"Transaction interacting with Uniswap Router:", tx['to'], "Tx_Hash: ",w3.to_hex(hash))
-                    print(style.GREEN+"Decoded Input Data:", decode_input_data(tx['input'],tx['to']))
-                    print(style.RED+"------------------")
+                    if tx['to'] == router_address:
+
+                        function_name = decode_input_data(tx['input'], tx['to'])
+                        if function_name is not None:
+                            print(style.YELLOW + "Transaction interacting with Uniswap Router:", tx['to'], "Tx_Hash: ",
+                                  w3.to_hex(hash))
+
+                            print(style.GREEN+f"Decoded Input Data: {function_name}")
+                            print(style.RED+"------------------")
+
         except Exception as e:
             print("An error occurred while processing a transaction:", e)
 
