978befed77f8eeccbe46f33882aec9f9753fac64
Henry Tirla <henrytirla@gmail.com>
2023-07-30 21:20:57 +0200

    Added Days filter for each transaction

--- Ethereum/Memepool/AllrouterAddresses.py
+++ Ethereum/Memepool/AllrouterAddresses.py
@@ -4,7 +4,6 @@
 
 import asyncio
 import time
-import threading
 from web3 import Web3
 import json
 import requests
@@ -29,7 +28,7 @@ class style():  # Class of different text colours - default is white
 
 w3 = Web3(Web3.HTTPProvider("https://eth-mainnet.g.alchemy.com/v2/lTlatSTYDZmCv6wVLRIDff7S3kZhL2dq"))
 
-
+txn_days=int(input("Fileter for transaction eg 1 All transaction less than 1 day ago/24Hr: "))
 uniswap_router_addresses = [
     '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',  #Uniswap-V2 Router
     '0xE592427A0AEce92De3Edee1F18E0157C05861564',  #Uniswap-V3 Router
@@ -46,6 +45,8 @@ check_functions = {"swapETHForExactTokens": True, "swapExactETHForTokens": True,
 #swapExactTokensForTokens
 check_functionName= {"V2_SWAP_EXACT_IN":True,"UNWRAP_WETH":True,"multicall":True}#"exactInputSingle":True,
 
+api_key = 'QSD4D9KG1NYTX3Y6CPAR62G9FBW16UZ81Z'
+
 
 def get_FunctionName(decoded_data):
     decoded_str = str(decoded_data[0])
@@ -58,49 +59,71 @@ def get_FunctionName(decoded_data):
 def decode_execute(decode_data,function_name):
     codec = RouterCodec()
     decoded_trx_input = codec.decode.function_input(decode_data)
-    check_V2= decoded_trx_input[1]['inputs'][0]
-    v2fnction= get_FunctionName(check_V2)
-
-    values_to_check = {"V3_SWAP_EXACT_IN", "V2_SWAP_EXACT_IN", "V3_SWAP_EXACT_OUT","V2_SWAP_EXACT_OUT","exactOutput"}
-
+    check_V2 = decoded_trx_input[1]['inputs'][0]
+    v2fnction = get_FunctionName(check_V2)
+    #print("V2 Function",v2fnction)
+    values_to_check = {"V3_SWAP_EXACT_IN", "V2_SWAP_EXACT_IN", "V3_SWAP_EXACT_OUT", "V2_SWAP_EXACT_OUT", "exactOutput"}
     if v2fnction not in values_to_check:
-        In_put = decoded_trx_input[1]['inputs'][1]
-        fn_name = get_FunctionName(In_put)
-        if fn_name in check_functionName and check_functionName[fn_name]:
-            if fn_name == 'V2_SWAP_EXACT_IN':
-                v2_path = In_put[1]['path']
-                if v2_path[0]== "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2":
-                    # print(style.CYAN + function_name)
-                    # print("Function Name: ", fn_name)
-                    token_address= v2_path[1]
-                    token_abi_json = get_contract_abi(token_address)
-                    if token_abi_json:
-                       token_abi = json.loads(token_abi_json)
-                       token_name, token_symbol = get_token_name_symbol(w3, token_address, token_abi)
-                       token_name, token_symbol = get_token_name_symbol(w3, token_address, token_abi)
-                       creation_date = get_contract_creation_date(token_address)
-                       time_since_creation = format_time_difference(creation_date)
-                       return f"Token Addess {token_address},{style.MAGENTA}Token Name/Symbol {token_name, token_symbol}, {style.CYAN}Creation Time {time_since_creation}"
+                In_put = decoded_trx_input[1]['inputs'][1]
+                fn_name = get_FunctionName(In_put)
+                if fn_name in check_functionName and check_functionName[fn_name]:
+                    if fn_name == 'V2_SWAP_EXACT_IN':
+                        v2_path = In_put[1]['path']
+                        if v2_path[0]== "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2":
+                            # print(style.CYAN + function_name)
+                            # print("Function Name: ", fn_name)
+                            token_address= v2_path[1] #
+                            creation_time=get_contract_creation_date(token_address)
+                            creation_day= get_Days(creation_time)
+                            if creation_day < txn_days:
+                                token_abi_json = get_contract_abi(token_address)
+                                if token_abi_json:
+                                   token_abi = json.loads(token_abi_json)
+                                   token_name, token_symbol = get_token_name_symbol(w3, token_address, token_abi)
+                                   token_name, token_symbol = get_token_name_symbol(w3, token_address, token_abi)
+                                   creation_date = get_contract_creation_date(token_address)
+                                   time_since_creation = format_time_difference(creation_date)
+                                   return f"Token Addess {token_address},{style.MAGENTA}Token Name/Symbol {token_name, token_symbol}, {style.CYAN}Creation Time {time_since_creation}"
 
 
-            elif fn_name == 'UNWRAP_WETH':
-                v3_data = decoded_trx_input[1]['inputs'][0]
-                if get_FunctionName(v3_data) == 'V3_SWAP_EXACT_IN':
-                    path = decoded_trx_input[1]['inputs'][0][1]['path']
-                    decoded_path = codec.decode.v3_path('V3_SWAP_EXACT_IN', path)
-                    return decoded_path
+                    elif fn_name == 'UNWRAP_WETH':
+                        v3_data = decoded_trx_input[1]['inputs'][0]
+                        if get_FunctionName(v3_data) == 'V3_SWAP_EXACT_IN':
+                            path = decoded_trx_input[1]['inputs'][0][1]['path']
+                            decoded_path = codec.decode.v3_path('V3_SWAP_EXACT_IN', path)
+                            return decoded_path
+                        else:
+                            #print(fn_name,decoded_trx_input)
+                            data = decoded_trx_input[1]['inputs'][0][1]['path']
+                            return fn_name, data
                 else:
-                    #print(fn_name,decoded_trx_input)
-                    data = decoded_trx_input[1]['inputs'][0][1]['path']
-                    return fn_name, data
-        else:
-            return None
+                    return None
     else:
-        return None
-
-api_key = 'QSD4D9KG1NYTX3Y6CPAR62G9FBW16UZ81Z'
+              return None
 
 
+def decode_multicall(multicall_decoded):
+    Inner_func = get_FunctionName(multicall_decoded)
+    values_to_check = {"swapExactTokensForTokens"}
+    #print("Inner Multicall Function ------",Inner_func)
+    if Inner_func not in values_to_check:
+        # print(style.CYAN + function_name)
+        tokens = [multicall_decoded[1]['params']['tokenOut'], multicall_decoded[1]['params']['tokenIn']]
+        if tokens[0] == '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2':
+
+            token_address = tokens[1]
+            creation_time = get_contract_creation_date(token_address)
+            creation_day = get_Days(creation_time)
+            if creation_day < txn_days:
+                token_abi_json = get_contract_abi(token_address)
+                if token_abi_json:
+                    token_abi = json.loads(token_abi_json)
+                    token_name, token_symbol = get_token_name_symbol(w3, token_address, token_abi)
+                    creation_date = get_contract_creation_date(token_address)
+                    time_since_creation = format_time_difference(creation_date)
+                    return f"Token Addess {token_address},{style.MAGENTA}Token Name/Symbol {token_name, token_symbol}, {style.CYAN}Creation Time {time_since_creation}"
+            else:
+                return None
 
 def get_contract_abi(contract_address):
     try:
@@ -136,6 +159,23 @@ def get_contract_creation_date(contract_address):
     else:
         raise Exception('Error while fetching transactions: ' + data['message'])
 
+
+def get_Days(creation_time_str):
+    from datetime import datetime, timedelta
+
+    creation_time = datetime.strptime(creation_time_str, "%d-%m-%Y %H:%M:%S")
+    current_time = datetime.utcnow()
+    time_difference = current_time - creation_time
+
+    # Extract days, hours, and minutes from the time difference
+    days = time_difference.days
+    hours, remainder = divmod(time_difference.seconds, 3600)
+    minutes, _ = divmod(remainder, 60)
+
+    return days
+
+
+
 def format_time_difference(creation_time_str):
     from datetime import datetime, timedelta
 
@@ -158,7 +198,7 @@ def get_token_name_symbol(web3, contract_address, abi):
 
     return token_name, token_symbol
 
-def decode_input_data(input_data, router):
+def decode_input_data(input_data, router,tx_hash):
     try:
         with open("router_abi.json") as f:
             routers_abi = json.load(f)
@@ -178,25 +218,12 @@ def decode_input_data(input_data, router):
 
                 byte_data = decoded_data[1]['data'][0]
                 multicall_decoded = contract.decode_function_input(byte_data)
-                Inner_func = get_FunctionName(multicall_decoded)
-                values_to_check = {"swapExactTokensForTokens"}
-                #print("Inner Multicall Function ------",Inner_func)
-                if Inner_func not in values_to_check:
-                   #print(style.CYAN + function_name)
-                   tokens = [multicall_decoded[1]['params']['tokenOut'], multicall_decoded[1]['params']['tokenIn']]
-                   if tokens[0]=='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2':
-
-                      token_address = tokens[1]
-                      token_abi_json = get_contract_abi(token_address)
-                      if token_abi_json:
-                          token_abi = json.loads(token_abi_json)
-                          token_name, token_symbol = get_token_name_symbol(w3, token_address, token_abi)
-                          creation_date= get_contract_creation_date(token_address)
-                          time_since_creation = format_time_difference(creation_date)
-                          return f"Token Addess {token_address},{style.MAGENTA}Token Name/Symbol {token_name, token_symbol}, {style.CYAN}Creation Time {time_since_creation}"
+                data=decode_multicall(multicall_decoded)
+                return data
 
             elif function_name == "addLiquidityETH":
-                 #print(style.CYAN+function_name)
+                 print(style.CYAN+function_name)
+                 print(decoded_data)
                  token_address=decoded_data[1]['token']
                  token_abi_json = get_contract_abi(token_address)
                  if token_abi_json:
@@ -212,21 +239,24 @@ def decode_input_data(input_data, router):
                 path_ =decoded_data[1]['path']
                 if path_[0] == '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2':
                     token_address = path_[1]
-                    token_abi_json = get_contract_abi(token_address)
-                    if token_abi_json:
-                        token_abi = json.loads(token_abi_json)
-                        token_name, token_symbol = get_token_name_symbol(w3, token_address, token_abi)
-                        creation_date = get_contract_creation_date(token_address)
-                        time_since_creation = format_time_difference(creation_date)
+                    creation_time = get_contract_creation_date(token_address)
+                    creation_day = get_Days(creation_time)
+                    if creation_day < txn_days:
+                        token_abi_json = get_contract_abi(token_address)
+                        if token_abi_json:
+                            token_abi = json.loads(token_abi_json)
+                            token_name, token_symbol = get_token_name_symbol(w3, token_address, token_abi)
+                            creation_date = get_contract_creation_date(token_address)
+                            time_since_creation = format_time_difference(creation_date)
 
-                        return f"Token Addess {token_address},{style.MAGENTA}Token Name/Symbol {token_name, token_symbol}, {style.CYAN}Creation Time {time_since_creation}"
+                            return f"Token Addess {token_address},{style.MAGENTA}Token Name/Symbol {token_name, token_symbol}, {style.CYAN}Creation Time {time_since_creation}"
+                    else:
+                        pass
     except IndexError:
-        # Handle the IndexError here (e.g., print a warning message)
         print("An error occurred while decoding the input data. The list index is out of range.")
         return None
     except Exception as e:
-        # Handle other exceptions here (e.g., print an error message)
-        print("An error occurred while decoding the input data:", e)
+        print("An error occurred while decoding the input data:", e,router,tx_hash)
         return None
 
 
@@ -235,7 +265,12 @@ async def handle_new_block(block):
     global count
     for tx_hash in block['transactions']:
         try:
+
             tx = w3.eth.get_transaction(tx_hash)
+            # creation_date = get_contract_creation_date(token_contract_address)
+            # days, time_since_creation = format_time_difference(creation_date)
+            #
+            # if days < 50:
 
 
             for router_address in uniswap_router_addresses:
@@ -243,12 +278,14 @@ async def handle_new_block(block):
                 if tx['to'] == router_address:
                     hash= tx['hash']
                     tx_hash = w3.to_hex(hash)
-                    function_name = decode_input_data(tx['input'], tx['to'])
+                    function_name = decode_input_data(tx['input'], tx['to'],tx_hash)
+                    #print("Function Name?",function_name)
 
                     if function_name is not None:
                         count += 1
                         tx_details= w3.eth.get_transaction(tx_hash)
-                        print(style.CYAN+f"Current Block Number: {tx_details['blockNumber']}, Latest Block Number: {w3.eth.block_number}",end='\n ')
+                        # print()tx_details['blockNumber']
+                        print(style.CYAN+f"Current Block Number: {block['number']}, Latest Block Number: {w3.eth.block_number}",end='\n ')
                         #print(print_block_numbers())
                         print(count,style.YELLOW + "Tx_Hash: ",
                               "https://etherscan.io/tx/"+tx_hash)
@@ -277,7 +314,7 @@ async def track_new_blocks():
             current_block_number = latest_block_number
 
         # Delay between checking for new blocks
-        await asyncio.sleep(2)
+        await asyncio.sleep(1)
 
 # Run the event loop
 
