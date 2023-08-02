0058821db3dfcd471412f4a3af7b18b72687d239
Henry Tirla <henrytirla@gmail.com>
2023-08-02 13:39:58 +0200

    TrackLockEvents #Todo

--- Ethereum/Trading/TrackLockEvents.py
+++ Ethereum/Trading/TrackLockEvents.py
@@ -3,3 +3,4 @@
 # How to track lock events of Popular locked Contracts, TrustSwap Security, Pinky, UNICRYPT Security
 # If lock > 100Year Buy Token
 # Write function to find contract address from LP -Addess
+# Inspired by Test Token
