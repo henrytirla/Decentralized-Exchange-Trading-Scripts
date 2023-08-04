31ea27b2d23ffd4cae9b5ddb81863310ff3fddb4
Henry Tirla <henrytirla@gmail.com>
2023-08-04 03:51:21 +0200

    Major improvements

--- Ethereum/Memepool/memepool2.0.py
+++ Ethereum/Memepool/memepool2.0.py
@@ -1,5 +1,6 @@
 
 from web3 import Web3
+import requests
 w3 = Web3(Web3.HTTPProvider("https://eth-mainnet.g.alchemy.com/v2/lTlatSTYDZmCv6wVLRIDff7S3kZhL2dq"))
 import time
 class style():  # Class of different text colours - default is white
@@ -18,35 +19,145 @@ WETH_ABI = '[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","t
 
 weth_contract = w3.eth.contract(address=WETH_ADDRESS, abi=WETH_ABI)
 
-uniswap_router_addresses = [
-    '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',  #Uniswap-V2 Router
-    '0xE592427A0AEce92De3Edee1F18E0157C05861564',  #Uniswap-V3 Router
-    '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45',  #Uniswap-V3 02 Router
-    '0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B', #OldUniversal-Router
-    '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD', #Universal-Router
-    ]
+uniswap_router_addresses = {'0x7a250d5630b4cf539739df2c5dacb4c659f2488d': True, #Uniswap-V2 Router
+                            '0xe592427a0aece92de3edee1f18e0157c05861564': True, #Uniswap-V3 Router
+                            '0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45': True, #Uniswap-V3 02 Router
+                            '0xef1c6e67703c7bd7107eed8303fbe6ec2554bf6b': True, #OldUniversal-Router
+                            '0x3fc91a3afd70395cd496c647d5a6cc9d4b2b7fad': True  #Universal-Router
+
+                            }
 
 
-block = w3.eth.get_block('latest')
+w3 = Web3(Web3.HTTPProvider("https://eth-mainnet.g.alchemy.com/v2/lTlatSTYDZmCv6wVLRIDff7S3kZhL2dq"))
+
+
+uniswap_v2_pair_abi = '[{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1","type":"uint256"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"Burn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0In","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1In","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount0Out","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1Out","type":"uint256"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"Swap","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint112","name":"reserve0","type":"uint112"},{"indexed":false,"internalType":"uint112","name":"reserve1","type":"uint112"}],"name":"Sync","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"MINIMUM_LIQUIDITY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"PERMIT_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"burn","outputs":[{"internalType":"uint256","name":"amount0","type":"uint256"},{"internalType":"uint256","name":"amount1","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"factory","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getReserves","outputs":[{"internalType":"uint112","name":"_reserve0","type":"uint112"},{"internalType":"uint112","name":"_reserve1","type":"uint112"},{"internalType":"uint32","name":"_blockTimestampLast","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_token0","type":"address"},{"internalType":"address","name":"_token1","type":"address"}],"name":"initialize","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"kLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"mint","outputs":[{"internalType":"uint256","name":"liquidity","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"price0CumulativeLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"price1CumulativeLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"skim","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"amount0Out","type":"uint256"},{"internalType":"uint256","name":"amount1Out","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"swap","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"sync","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"token0","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"token1","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]'
+
+
+
+
+def calculate_time_difference(creation_time_str):
+    from datetime import datetime, timedelta
+
+    creation_time = datetime.strptime(creation_time_str, "%d-%m-%Y %H:%M:%S")
+    current_time = datetime.utcnow()
+    time_difference = current_time - creation_time
+
+    # Extract days, hours, and minutes from the time difference
+    days = time_difference.days
+    hours, remainder = divmod(time_difference.seconds, 3600)
+    minutes, _ = divmod(remainder, 60)
+
+    return f"{days} days {hours} hours {minutes} minutes ago"
+
+def get_contract_creation_date(contract_address):
+    from datetime import datetime
+    url = f'https://api.etherscan.io/api?module=account&action=txlist&address={contract_address}&startblock=0&endblock=99999999&page=1&offset=3&sort=asc&apikey=QSD4D9KG1NYTX3Y6CPAR62G9FBW16UZ81Z'
+    response = requests.get(url)
+    data = response.json()
+
+    if data['status'] == '1':
+        for tx in data['result']:
+            if tx['to'] == contract_address.lower():
+                timestamp = int(tx['timeStamp'])
+                formatted_time = datetime.utcfromtimestamp(timestamp).strftime("%d-%m-%Y %H:%M:%S")
+                return formatted_time
+    else:
+        raise Exception('Error while fetching transactions: ' + data['message'])
+
+def get_Days(creation_time_str):
+    from datetime import datetime, timedelta
+
+    creation_time = datetime.strptime(creation_time_str, "%d-%m-%Y %H:%M:%S")
+    current_time = datetime.utcnow()
+    time_difference = current_time - creation_time
+
+    # Extract days, hours, and minutes from the time difference
+    days = time_difference.days
+    hours, remainder = divmod(time_difference.seconds, 3600)
+    minutes, _ = divmod(remainder, 60)
+
+    return days
+
+def get_token_from_lp(lpAddres):
+    lpAddres= w3.to_checksum_address(lpAddres)
+    uniswap_v2_pair = w3.eth.contract(address=lpAddres, abi=uniswap_v2_pair_abi)
+
+    tokenA_address = uniswap_v2_pair.functions.token0().call()
+    tokenB_address = uniswap_v2_pair.functions.token1().call()
+
+    if tokenA_address != WETH_ADDRESS:
+        creation_date= get_contract_creation_date(tokenA_address)
+        #num_days=get_Days(creation_date)
+        #time_diff= calculate_time_difference(creation_date)
+        return tokenA_address
+    else:
+        creation_date= get_contract_creation_date(tokenA_address)
+        # num_days=get_Days(creation_date)
+        # time_diff= calculate_time_difference(creation_date)
+
+        return tokenB_address
 
 params = {"fromBlock": w3.eth.block_number}
+
 logs = weth_contract.events.Transfer().get_logs(params)
 
-#0xdAC17F958D2ee523a2206206994597C13D831ec7 Tether
-while True:
-    params = {"fromBlock": w3.eth.block_number}
 
-    block = w3.eth.block_number
-
-    logs = weth_contract.events.Transfer().get_logs(params)
-    print(style.RED + f"{logs[0]['blockNumber']}")
-
+def process_logs(logs):
     for log in logs:
-        if log.args.src.lower() in [addr.lower() for addr in uniswap_router_addresses]:
-            print(
-                style.RESET+f"Transfer of {round(w3.from_wei(log.args.wad, 'ether'),2)} WETH from {log.args.src} to {log.args.dst}, Transaction Hash: {w3.to_hex(log.transactionHash)}")
+        src_address = log.args.src.lower()
+        dst_address = log.args.dst.lower()
 
-    time.sleep(8)
+        if src_address in router_addresses_set and dst_address not in router_addresses_set:
+            token_contract = get_token_from_lp(dst_address)
+            creation_date = get_contract_creation_date(token_contract)
+            num_days = get_Days(creation_date)
+            time_diff = calculate_time_difference(creation_date)
+            creation_date = get_contract_creation_date(token_contract)
+            num_days=get_Days(creation_date)
+            time_diff= calculate_time_difference(creation_date)
+
+            if num_days < 1:
+                print("Token Bought", token_contract, "Creation Time: ", time_diff)
+
+                print(f"Transfer of {round(w3.from_wei(log.args.wad, 'ether'), 2)} WETH from {src_address} to {dst_address}, Transaction Hash: https://etherscan.io/tx/{w3.to_hex(log.transactionHash)}")
+
+                print("---------")
+            else:
+                continue
+
+router_addresses_set = set(uniswap_router_addresses)
+
+latest_block_number = w3.eth.block_number
+
+while True:
+    try:
+        start_time = time.time()
+
+
+        logs = weth_contract.events.Transfer().get_logs(params)
+        print(style.RED + f"Current Block Scan: {logs[0]['blockNumber']}")
+        print(style.RESET)
+        print(style.GREEN+f"Latest Block Scan: {latest_block_number}")
+        print(style.RESET)
+        if logs:
+            # Process the logs
+            process_logs(logs)
+
+            # Update the latest block number
+            latest_block_number = logs[-1].blockNumber + 1
+        execution_time = time.time() - start_time
+
+        if execution_time < 5:
+            # Wait for 3 more seconds before checking for new blocks again
+            time.sleep(5 - execution_time)
+        elif execution_time > 8:
+            # Process new logs immediately
+            continue
+    except Exception as e:
+        print("An error occurred while processing a transaction:", e)
+
+
 
 
 
