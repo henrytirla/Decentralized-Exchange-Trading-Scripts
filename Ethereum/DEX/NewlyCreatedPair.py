6129002bf4ea65303026ad0194060d37a07607a2
Henry Tirla <henrytirla@gmail.com>
2023-08-10 05:58:07 +0200

    Newly Created Pairs

---
+++ Ethereum/DEX/NewlyCreatedPair.py
@@ -0,0 +1,101 @@
+import json
+from web3 import Web3
+import time
+import datetime
+import requests
+
+
+
+class style():  # Class of different text colours - default is white
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+
+
+
+
+def getTimestamp():
+    while True:
+        timeStampData = datetime.datetime.now()
+        currentTimeStamp = "[" + timeStampData.strftime("%H:%M:%S.%f")[:-3] + "]"
+        return currentTimeStamp
+
+
+web3 = Web3(Web3.HTTPProvider("https://eth-mainnet.g.alchemy.com/v2/lTlatSTYDZmCv6wVLRIDff7S3kZhL2dq"))
+if web3.is_connected():
+    print(getTimestamp()," Ethereum Node successfully connected")
+
+
+
+#
+# pancakeswap factory
+pancake_factory = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'  # Testnet  #0x6725F303b657a9451d8BA641348b6761A6CC7a17
+pancake_factory_abi = json.loads('[{"inputs":[{"internalType":"address","name":"_feeToSetter","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"token0","type":"address"},{"indexed":true,"internalType":"address","name":"token1","type":"address"},{"indexed":false,"internalType":"address","name":"pair","type":"address"},{"indexed":false,"internalType":"uint256","name":"","type":"uint256"}],"name":"PairCreated","type":"event"},{"constant":true,"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"allPairs","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"allPairsLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"tokenA","type":"address"},{"internalType":"address","name":"tokenB","type":"address"}],"name":"createPair","outputs":[{"internalType":"address","name":"pair","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"feeTo","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"feeToSetter","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"getPair","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_feeTo","type":"address"}],"name":"setFeeTo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_feeToSetter","type":"address"}],"name":"setFeeToSetter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]')
+contract = web3.eth.contract(address=pancake_factory, abi=pancake_factory_abi)
+"""Connection to Pancakeswap"""
+
+tokenNameABI = json.loads(
+    '[ { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "spender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" } ], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" } ], "name": "Transfer", "type": "event" }, { "constant": true, "inputs": [ { "internalType": "address", "name": "_owner", "type": "address" }, { "internalType": "address", "name": "spender", "type": "address" } ], "name": "allowance", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "approve", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "internalType": "address", "name": "account", "type": "address" } ], "name": "balanceOf", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "getOwner", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "name", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "internalType": "address", "name": "recipient", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "transfer", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "internalType": "address", "name": "sender", "type": "address" }, { "internalType": "address", "name": "recipient", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "transferFrom", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" } ]')
+
+
+
+#web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+liquidityPairAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+
+#
+#event_filter = contract.events.PairCreated.create_filter(fromBlock=30716374, toBlock=30717588)
+event_filter = contract.events.PairCreated.create_filter(fromBlock="latest")
+
+while True:
+    try:
+
+        #events = event_filter.get_all_entries()
+        events = event_filter.get_new_entries()
+
+        if len(events) > 0:
+           for event in events:
+               if (event['args']['token1'] == "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"):
+                   tokenAddress = event['args']['token0']
+
+                   getTokenName = web3.eth.contract(address=tokenAddress,
+                                                    abi=tokenNameABI)
+                   tokenName = getTokenName.functions.name().call()
+                   tokenSymbol = getTokenName.functions.symbol().call()
+                   tx_hash = web3.to_hex(event['transactionHash'])
+                   print(
+                       style.YELLOW + getTimestamp() + " [Token] New Token Detected: " + style.CYAN + tokenName + " (" + tokenSymbol + "): " + style.MAGENTA + tokenAddress + " https://etherscan.io/tx/" + tx_hash + style.RESET)
+
+               elif (event['args']['token0'] == liquidityPairAddress):
+                   tokenAddress = event['args']['token1']
+                   getTokenName = web3.eth.contract(address=tokenAddress,
+                                                    abi=tokenNameABI)  # code to get name and symbol from token address
+                   tokenName = getTokenName.functions.name().call()
+                   tokenSymbol = getTokenName.functions.symbol().call()
+                   tx_hash = web3.to_hex(event['transactionHash'])
+
+                   print(
+                       style.RED + getTimestamp() + " [Token] New Token Detected: " + style.CYAN + tokenName + " (" + tokenSymbol + "): " + style.MAGENTA + tokenAddress + "  https://etherscan.io/tx/" + tx_hash + style.RESET)
+
+
+               else:
+                   # print(style.BLUE + "Not WBNB LQ PAIR")
+                   pass
+        time.sleep(1)
+
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+
+
+
+
