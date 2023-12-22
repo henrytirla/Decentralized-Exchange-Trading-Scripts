e756c55249c37a94065ec5eee373356d10ee310a
Henry Tirla <henrytirla@gmail.com>
2023-12-22 13:47:15 +0100

    Monitor pools concurrently

---
+++ Ethereum/Monitoring/Monitor-MultipleLp-Concurrently.py
@@ -0,0 +1,10 @@
+"""Monitor multiple liquidity providers concurrently and send alerts to telegram bot. Have a basis for growth rate and decline rate."
+
+How can this be intergrated to the evaluabtion bot?
+Can you get traction of a token and determine optimal buying point and selling point
+
+What data do you need to determine the optimal buying point and selling point?
+
+What is the best way to determine the optimal buying point and selling point?
+
+"""
