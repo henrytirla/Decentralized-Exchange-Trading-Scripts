fdfae5b6c1edfa93eb66d080b885cb655bcdf5cd
Henry Tirla <henrytirla@gmail.com>
2024-01-14 17:28:36 +0100

    Update NewlyCreatedPairs.py
    Detect  New Pools Created on Solana Raydium DEX

--- Solana/NewlyCreatedPairs.py
+++ Solana/NewlyCreatedPairs.py
@@ -1,11 +1,83 @@
-"V1 Script to get newly created pairs on Raydium    "
+"Detect  New Pools Created on Solana Raydium DEX"
 
+import asyncio
+import sys
+
+import websockets
+import json
 from solana.rpc.api import Client
-
 from solders.pubkey import Pubkey
-from solders.transaction import Transaction
 from solders.signature import Signature
-RAYDIUM_PROGRAM_ID = Pubkey.from_string("675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8")
+import pandas as pd
+from tabulate import tabulate
 
-OPENBOOK = Pubkey.from_string("srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX")
-METADATA_2022_PROGRAM_ID = Pubkey.from_string("META4s4fSmpkTbZoUsgC1oBnWB31vQcmnN8giPw51Zu")
+
+wallet_address = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8"
+seen_signatures = set()
+solana_client = Client("https://api.mainnet-beta.solana.com")
+
+def  getTokens(str_signature):
+    signature = Signature.from_string(str_signature)
+    transaction = solana_client.get_transaction(signature, encoding="jsonParsed",max_supported_transaction_version=0).value
+    instruction_list = transaction.transaction.transaction.message.instructions
+    if any("initialize2" in message for message in transaction.transaction.meta.log_messages):
+        for instructions in instruction_list:
+            if instructions.program_id == Pubkey.from_string(wallet_address):
+                print("============NEW POOL DETECTED====================")
+                Token0= instructions.accounts[8]
+                Token1= instructions.accounts[9]
+                # Your data
+                data = {'Token_Index': ['Token0', 'Token1'],
+                        'Account Public Key': [Token0, Token1]}
+
+                df = pd.DataFrame(data)
+                table = tabulate(df, headers='keys', tablefmt='fancy_grid')
+                print(table)
+             
+
+
+async def run():
+   uri = "wss://api.mainnet-beta.solana.com"
+   async with websockets.connect(uri) as websocket:
+       # Send subscription request
+       await websocket.send(json.dumps({
+           "jsonrpc": "2.0",
+           "id": 1,
+           "method": "logsSubscribe",
+           "params": [
+               {"mentions": [wallet_address]},
+               {"commitment": "finalized"}
+           ]
+       }))
+
+       first_resp = await websocket.recv()
+       response_dict = json.loads(first_resp)
+       if 'result' in response_dict:
+          print("Subscription successful. Subscription ID: ", response_dict['result'])
+
+       # Continuously read from the WebSocket
+       async for response in websocket:
+          
+           response_dict = json.loads(response)
+
+           if response_dict['params']['result']['value']['err'] == None :
+               signature = response_dict['params']['result']['value']['signature']
+
+               if signature not in seen_signatures:
+                  seen_signatures.add(signature)
+                  log_messages_set = set(response_dict['params']['result']['value']['logs'])
+
+                  search="initialize2"
+                  if any(search in message for message in log_messages_set):
+                      print("True",signature)
+                      getTokens(signature)
+
+                 
+           else:
+               pass
+
+
+async def main():
+    await run()
+
+asyncio.run(main())
