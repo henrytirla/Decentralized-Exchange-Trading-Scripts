61db668518d18a5082fe041bc93ac187bc4b8a43
Henry Tirla <henrytirla@gmail.com>
2024-01-25 20:10:55 +0100

    Get public key from privateKey Str

--- Solana/Wallet/WalletFrom_PrivateKey.py
+++ Solana/Wallet/WalletFrom_PrivateKey.py
@@ -1,4 +1,6 @@
+import base58
 from base58  import b58decode, b58encode
+from solana.rpc.api import  Keypair
 
 #Wallet Address:  FTvNm4CQPkqcUB6AL9SE5TS8KswCfr9xGj47dzTT24CU
 #Private Key:  21T8iLRJpWterSN1ozw4sEoaZ1aYVAQWcWmESisiyjQuEitXNF3PcDJqNxdbx7bTqL9wGQKhAhaMB7JFekmKK2Lk
@@ -7,12 +9,17 @@ from base58  import b58decode, b58encode
 encoded_pair= "21T8iLRJpWterSN1ozw4sEoaZ1aYVAQWcWmESisiyjQuEitXNF3PcDJqNxdbx7bTqL9wGQKhAhaMB7JFekmKK2Lk"
 key_pair= b58decode(encoded_pair)
 
-private_key= key_pair[:32]
-public_key= key_pair[32:]
+private_key_bytes = base58.b58decode(encoded_pair)
+# Create a Keypair object from the secret key bytes
+keypair = Keypair.from_bytes(private_key_bytes)
+print(keypair.pubkey())
 
-print("Wallet Address: ",b58encode(public_key).decode())
-
-# To get the private key from the keypair,
-_keypair= private_key+public_key
-_encoded_keypair= b58encode(_keypair).decode()
-print("Private Key: ",_encoded_keypair)
+# private_key= key_pair[:32]
+# public_key= key_pair[32:]
+#
+# print("Wallet Address: ",b58encode(public_key).decode())
+#
+# # To get the private key from the keypair,
+# _keypair= private_key+public_key
+# _encoded_keypair= b58encode(_keypair).decode()
+# print("Private Key: ",_encoded_keypair)
