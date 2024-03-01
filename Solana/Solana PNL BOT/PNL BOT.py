7201826969d03e83c1a3f8e03db63526341f3d56
Henry Tirla <henrytirla@gmail.com>
2024-03-01 14:36:56 +0100

    Solana PNL Bot

---
+++ Solana/Solana PNL BOT/PNL BOT.py
@@ -0,0 +1,9 @@
+"""
+
+This bot gets the PNL of a Wallet over custom periods eg 7 days, 14 days, 30 days, 60 days, and 90 days. This helps you see if there is progress or regress in a wallet strategy giving you confidence to engage in copytrading and help you gauge your capital.
+
+Generated Reports: https://drive.google.com/drive/folders/15INgNO8p9A30Ma5gaKzVlz8ZytV8Jmom
+
+https://www.youtube.com/watch?v=NGNwHvu7BkY&t=231s
+
+"""
